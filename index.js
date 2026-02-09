b2630d3dabf5e8386af1e552e504ba71dd5433a9
Kent1an <qkent@anamosa.k12.ia.us>
2026-02-09 18:20:02 +0000

    done

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 var lengthInFeet = 1.5;
 var pricePerFoot = 2.50;
-var totalPrice = lengthinFeet * pricePerFoot;
+var totalPrice = lengthInFeet * pricePerFoot;
 document.getElementById('result').innerHTML = totalPrice;
